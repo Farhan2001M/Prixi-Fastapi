2316238ecf651d23917b3bae1a2ca256a60c6815
farhan <chfarhanilyas550@gmail.com>
2024-12-30 04:22:57 +0500

    fourth check

--- app/api.py
+++ app/api.py
@@ -1,25 +1,25 @@
 from fastapi import FastAPI 
-from fastapi.middleware.cors import CORSMiddleware
+# from fastapi.middleware.cors import CORSMiddleware
 
-from .routes.adminRoutes import router as admin_router
+# from .routes.adminRoutes import router as admin_router
 
 app = FastAPI()
 
-origins = ["http://localhost:3000",
-           "https://localhost:3000",
-           "http://localhost:3001",
-           "https://localhost:3001",
-          ]
+# origins = ["http://localhost:3000",
+#            "https://localhost:3000",
+#            "http://localhost:3001",
+#            "https://localhost:3001",
+#           ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins = origins,
-    allow_credentials = True,
-    allow_methods = ["*"],
-    allow_headers = ["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins = origins,
+#     allow_credentials = True,
+#     allow_methods = ["*"],
+#     allow_headers = ["*"],
+# )
 
-app.include_router(admin_router)
+# app.include_router(admin_router)
 
 
 @app.get("/")
