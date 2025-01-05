dc376e193eef435485e842f7d8243a9da40f8677
farhan <chfarhanilyas550@gmail.com>
2025-01-05 15:52:59 +0500

    Updated env

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-
 import uvicorn
+import os
 
-PORT = 8000
+# Get the port from the environment variable, fallback to 8000 if not set
+PORT = int(os.getenv('PORT', 8000))
 HOST = '127.0.0.1'
 
 if __name__ == '__main__':
-    uvicorn.run('app.api:app', host = HOST, port = PORT, reload = True)
+    uvicorn.run('app.api:app', host=HOST, port=PORT, reload=True)
